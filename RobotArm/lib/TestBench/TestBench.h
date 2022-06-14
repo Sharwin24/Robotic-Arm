8724c29d219497a876bf90a4c2204103d9888155
Sharwin Patil <sharwin24@gmail.com>
2022-06-14 03:44:38 -0400

    Moved TestBench code

--- RobotArm/lib/TestBench/TestBench.h
+++ RobotArm/lib/TestBench/TestBench.h
@@ -14,7 +14,16 @@ class TestBench {
         RobotArm = m;
     }
 
-    void testLinkMotors();
+    void testLinkMotors() {
+        RobotArm.link1ToAngle(0);
+        delay(1000);
+        RobotArm.link1ToAngle(90);
+        delay(1000);
+        RobotArm.link1ToAngle(115);
+        delay(1000);
+        RobotArm.link1ToAngle(0);
+        delay(1000);
+    }
 };
 
 #endif
