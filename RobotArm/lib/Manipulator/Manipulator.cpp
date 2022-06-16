2d98f33ca579fc63e71ef97ecfe0233c636aca85
Sharwin Patil <sharwin24@gmail.com>
2022-06-15 23:48:47 -0400

    Added updater for EE after movement

--- RobotArm/lib/Manipulator/Manipulator.cpp
+++ RobotArm/lib/Manipulator/Manipulator.cpp
@@ -68,6 +68,8 @@ void Manipulator::moveToAngles(float q1, float q2, float q3) {
     Link3.setTarget(q3Steps);
     // Update all Links until target is acheived
     updateLinks();
+    // Update EE pos
+    EEpos = ForwardKinematics(q1, q2, q3);
 }
 
 void Manipulator::moveToJointAngles(JointAngles jointAngleTargets) {
